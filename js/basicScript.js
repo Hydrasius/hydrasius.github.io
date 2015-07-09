b533ba4cf54e7783c696682ec1913403817b1cac
rsandu <razvi.sandu@gmail.com>
2015-07-09 01:41:06 -0600

    Clicking on image plays john cena audio file

--- js/basicScript.js
+++ js/basicScript.js
@@ -1,3 +1,19 @@
 /**
  * Created by spook_000 on 7/8/2015.
  */
+
+$(document).ready(function(){ // on document loadup
+    var audioElement = document.createElement('audio'); // create variable audioElement inheriting audio
+
+    audioElement.setAttribute('src', 'http://puu.sh/iIJqP/7ca580ba60.wav'); // setting source of audio
+
+    $.get();
+    audioElement.addEventListener('load', function(){ // on load of audioElement, add play function
+        audioElement.play();
+    }, true); // setting it to true makes it bubbled (read docs for document.ready();
+
+    $('img').click(function(){
+        event.preventDefault();
+        audioElement.play();
+    })
+});
